ea553e544694c639f693db7fad53a957614302db
Mihail Gaberov <mihail.gaberov@gmail.com>
2023-01-08 11:17:22 +0100

    :alembic: Playing with Winston options

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const winston = require('winston');
 
 const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
+  levels: winston.config.syslog.levels,
+  level: process.env.LOG_LEVEL || 'info',
+  format: winston.format.cli(),
   transports: [new winston.transports.Console()],
 });
 
-logger.info('Info message');
-logger.error('Error message');
-logger.warn('Warning message');
+
+
+// logger.info('Info message');
+// logger.error('Error message');
+// logger.warn('Warning message');
+
+logger.emerg("Emergency");
+logger.crit("Critical");
+logger.warning("Warning");
 
