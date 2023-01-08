28428a52ee95b8049a552232fd130479ef99fe6b
Mihail Gaberov <mihail.gaberov@gmail.com>
2023-01-08 13:05:33 +0100

    :student: Logging in an Express application using Winston and Morgan

--- server.js
+++ server.js
@@ -19,11 +19,22 @@ const logger = winston.createLogger({
 });
 
 const morganMiddleware = morgan(
-    ':method :url :status :res[content-length] - :response-time ms',
+    function (tokens, req, res) {
+      return JSON.stringify({
+        method: tokens.method(req, res),
+        url: tokens.url(req, res),
+        status: Number.parseFloat(tokens.status(req, res)),
+        content_length: tokens.res(req, res, 'content-length'),
+        response_time: Number.parseFloat(tokens['response-time'](req, res)),
+      });
+    },
     {
       stream: {
         // Configure Morgan to use our custom logger with the http severity
-        write: (message) => logger.http(message.trim()),
+        write: (message) => {
+          const data = JSON.parse(message);
+          logger.http(`incoming-request`, data);
+        },
       },
     }
 );
